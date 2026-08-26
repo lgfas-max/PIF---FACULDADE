c8764721422d4191837039965ec2fed0226083ff
Luann Flor <lgfas@cesar.school>
2026-08-25 22:17:27 -0300

    Delete ATIVIDADES/atividade 1/codigos directory

--- ATIVIDADES/atividade 1/codigos/Q19.C
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("um\n\tdois\n\t\ttres\n");
-    system("PAUSE");
-    return 0;
-}
--- ATIVIDADES/atividade 1/codigos/Q25.C
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-    printf("CCCCC\nC\nC\nCCCCC\n");
-    system("PAUSE");
-    return 0;
-}
